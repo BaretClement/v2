15717208d45c856c87d15b48769c5f791458ae9d
Clément Baret <clement.baret@gmail.com>
2017-05-17 15:31:40 +0200

    Add GA tag

--- wp-content/themes/unowr/footer.php
+++ wp-content/themes/unowr/footer.php
@@ -20,5 +20,14 @@
 	<?php wp_footer(); ?>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/merge-scripts.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KDMPQMJ');</script>
+<!-- End Google Tag Manager -->
+
 </body>
 </html>
